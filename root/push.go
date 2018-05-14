dc08a8aa5876956468d7076f233578ba586cd5f1
Stuart Harris <stuart.harris@red-badger.com>
2018-05-14 17:42:22 +0100

    image push

--- root/push.go
+++ root/push.go
@@ -1,3 +1,24 @@
 package root
 
-func push(ref string) {}
+import (
+	"context"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/pkg/errors"
+)
+
+func push(ref string) (err error) {
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return
+	}
+	rc, err := cli.ImagePush(ctx, ref, types.ImagePushOptions{})
+	if err != nil {
+		return errors.Wrap(err, "pushing image to repository")
+	}
+	defer rc.Close()
+	return streamDockerMessages(os.Stdout, rc)
+}
