39bf193c78922423d6aa131eb6008a1dd92bf656
Stuart Harris <stuart.harris@red-badger.com>
2018-05-22 14:58:05 +0100

    specify an optional cache registry

--- root/parseDockerfile.go
+++ root/parseDockerfile.go
@@ -11,29 +11,49 @@ import (
 )
 
 // parseDockerfile the Dockerfile into an array of stages
-func parseDockerfile(reader io.Reader, imgName string) (stages []string, images map[string]string, err error) {
+func parseDockerfile(reader io.Reader, imgName, cache string) (stages []string, imgNames map[string]string, err error) {
 	ast, err := parser.Parse(reader)
 	ss, _, err := instructions.Parse(ast.AST)
 	if err != nil {
 		return
 	}
 
-	imgNamed, err := reference.ParseNamed(imgName)
+	imgNamed, err := getNamed(imgName, cache)
 	if err != nil {
 		return
 	}
-
 	stage := 0
 	stages = make([]string, len(ss))
-	images = make(map[string]string)
-	for _, x := range ss {
-		name := x.Name
-		if name == "" {
-			name = strconv.Itoa(stage)
+	imgNames = make(map[string]string)
+	for _, s := range ss {
+		stageName := s.Name
+		if stageName == "" {
+			stageName = strconv.Itoa(stage)
 		}
-		stages[stage] = name
-		images[name] = fmt.Sprintf("%s-%s", reference.TrimNamed(imgNamed), name)
+		stages[stage] = stageName
+		named := reference.TrimNamed(imgNamed)
+		imgNames[stageName] = fmt.Sprintf("%s-%s", named, stageName)
 		stage++
+	}
+	return
+}
+
+func getNamed(imgName, cache string) (imgNamed reference.Named, err error) {
+	imgNamed, err = reference.ParseNamed(imgName)
+	if err != nil {
+		return
+	}
+	var (
+		domain  string
+		nameStr string
+	)
+	domain, nameStr = reference.SplitHostname(imgNamed)
+	if cache != "" {
+		domain = cache
+	}
+	imgNamed, err = reference.WithName(domain + "/" + nameStr)
+	if err != nil {
+		return
 	}
 	return
 }
